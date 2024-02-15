2b7be1c4fda3dacd45ff41f27a9ea1b49b2e7b18
tccmobile <William.smith@tulsacc.edu>
2024-02-15 16:27:18 +0000

    cmake works

---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/Person.cpp.o"
+  "CMakeFiles/MyExecutable.dir/main.cpp.o"
+  "MyExecutable"
+  "MyExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
